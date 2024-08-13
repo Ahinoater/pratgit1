f3ee5bf258e7d300d506a9d5abe3bb620bf5fc44
Desbye Tercero <terceroahinoa1@gmail.com>
2024-08-13 12:53:33 -0700

    Verificación de correo

---
+++ regex1.rb
@@ -0,0 +1,12 @@
+def validar_correo(correo)
+  regex = /\[\w+\-,]+@[a-z\d\-,]+\.[a-z]+\z/i
+  if correo.match?(regex)
+    puts "Correo Válido"
+  else 
+    puts "Correo Inválido"
+  end 
+end
+
+#Ejemplo de Uso
+validar_correo("ejemplo@dominio.com")
+validar_correo("mal.correo@")
